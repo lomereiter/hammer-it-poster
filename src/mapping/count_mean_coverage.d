76ffe27b1130aa213a238e202f2acee89ff93aec
atarasov <lomereiter@gmail.com>
2013-04-13 11:33:17 +0400

    mapping results for 100bp, 200bp, 300bp

--- src/mapping/count_mean_coverage.d
+++ src/mapping/count_mean_coverage.d
@@ -2,7 +2,7 @@ import bio.bam.reader, bio.bam.pileup, bio.bam.baseinfo;
 import std.stdio, std.parallelism, std.conv, std.range, std.algorithm;
 
 void main(string[] args) {
-  auto pool = new TaskPool(16);
+  auto pool = new TaskPool(4);
   scope(exit) pool.finish();
 
   auto bam = new BamReader(args[1], pool);
@@ -10,6 +10,8 @@ void main(string[] args) {
   auto n_total_bases = 0U;
   auto n_total_length = 0U;
   auto n_total_nonzero_coverage = 0U;
+
+  auto name_width = bam.reference_sequences.map!(s => s.name.length).reduce!max;
 
   foreach (int i, ref_seq; bam.reference_sequences) {
     n_total_length += ref_seq.length;
@@ -24,9 +26,15 @@ void main(string[] args) {
       n_total_nonzero_coverage += 1;
     }
 
-    writeln(ref_seq.name, '\t',
+    writeln(ref_seq.name, ' '.repeat(name_width - ref_seq.name.length), '\t',
             ref_seq.length, '\t',
             n_ref_nonzero_coverage, '\t',
-            n_ref_bases.to!double() / ref_seq.length);
+            n_ref_bases.to!double / ref_seq.length);
   }
+
+  writeln('='.repeat(40));
+  writeln(' '.repeat(name_width), '\t', 
+          n_total_length, '\t',
+          n_total_nonzero_coverage, '\t',
+          n_total_bases.to!double / n_total_length);
 }
